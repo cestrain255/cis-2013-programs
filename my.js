2b5a96a30df6c402ee0ace6908e6423715956c6b
Celie Strain <78047034+cestrain255@users.noreply.github.com>
2021-01-27 11:47:25 -0600

    Update my.js

--- my.js
+++ my.js
@@ -7,7 +7,7 @@ var mainProgram = function ()
 {
     var stringFirstName = prompt("What is your first name?");
     if (!isNaN(stringFirstName)) {
-        alert("default");
+        alert("You did not enter a valid first name, so I will call you Michael Scott.  Hello Michael Scott!");
     } else
     {
         alert("Hello "+ stringFirstName +"! It's great to meet you!");  
